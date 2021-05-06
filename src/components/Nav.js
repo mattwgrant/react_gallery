47e5ec7ccf4e00656530fea498afd9b442245917
Matthew Grant <amattwgrant@gmail.com>
2021-05-06 12:56:58 -0400

    my first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+	return (
+		<nav className="main-nav">
+	        <ul>
+	          <li><NavLink to='#'>Cats</NavLink></li>
+	          <li><NavLink to='#'>Cats</NavLink></li>
+	          <li><NavLink to='#'>Cats</NavLink></li>
+	        </ul>
+	    </nav>
+	);
+}
+
+export default Nav;
